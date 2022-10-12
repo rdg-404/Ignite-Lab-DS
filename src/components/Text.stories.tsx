5d21c4dcc713ec6771797c198aab13225aa3601c
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-10-12 18:35:41 -0300

    config the storybook layaout

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -5,8 +5,17 @@ export default{
     title: 'Components/Text',
     component: Text,
     args: {
-        children: 'Olá mundo'
+        children: 'Olá mundo',
+        size: 'md',
     },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
 } as Meta<TextProps>
 
 export const Default: StoryObj<TextProps> = {} 
@@ -29,5 +38,12 @@ export const CustomComponent: StoryObj<TextProps> = {
         children: (
             <p>Testando</p>
         )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        }
     }
 } 
