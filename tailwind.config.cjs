5bf3247b3641fa3da6e0c31d957f7bed923e3abe
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-10-12 17:20:22 -0300

    import tokens, font-size and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,26 @@ module.exports = {
     './src/**/*.tsx', //arquivos que irao receber estilização
   ],
   theme: {
+    fontSize:{
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gary-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+
+      'cyan-500': '#81d1f7',
+      'cyan-300': '#9be1fb'
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
