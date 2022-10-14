4537e7529519281ed3af08c7472636ef808eff3d
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-10-14 07:26:31 -0300

    add test in component signin

--- src/pages/Sign.stories.tsx
+++ src/pages/Sign.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { SignIn } from './SignIn';
 
 
@@ -9,4 +11,20 @@ export default{
     argTypes: {},
 } as Meta
 
-export const Default: StoryObj = {} 
+export const Default: StoryObj = {
+    play: async ({canvasElement}) => {
+        const canvas = within(canvasElement)
+
+        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'rodrigop@gmail.com') ///testa o email
+        userEvent.type(canvas.getByPlaceholderText('******'), '123456') ///testa a senha
+
+
+        userEvent.click(canvas.getByRole('button')) ///ao clicar no botao
+
+
+        ///fica retornando ate os texto aparecer na tela
+        await waitFor(() => {
+            return expect(canvas.getByText('Login Realizado')).toBeInTheDocument() 
+        })
+    }
+} 
